1ab9e9a3f64f5cdb02e8dcf5ee8195bbf33bc9c6
unknown <Adebisiazeez1997@gmail.com>
2025-08-27 16:39:59 +0100

    latest

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const BASE_URL=import.meta.NODE ==="development"?"http://localhost:5001/api": "/api"
-const api =axios.create({
-    baseURL:BASE_URL
-})
-export default api
+const BASE_URL =
+  import.meta.NODE === "development" ? "http://localhost:5001/api" : "/api";
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+export default api;
